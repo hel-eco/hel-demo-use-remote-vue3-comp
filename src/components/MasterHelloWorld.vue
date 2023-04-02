62fc413b894ed5d749e548b0bfec2eb85920cd56
fantasticsoul <zhongzhengkai@gmail.com>
2023-04-02 22:00:33 +0800

    feat: bump v4

--- src/components/MasterHelloWorld.vue
+++ src/components/MasterHelloWorld.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>render a remote vue comp in red box below</h3>
+    <div style="border:1px solid red;padding:6px">
     <RemoteComp name="hel remote vue3 comp" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import comps from 'hel-tpl-remote-vue3-comps-ts'
-
-console.log('see RemoteHelloWorld', comps.HelloWorld.name)
-console.log('see RemoteHelloWorld', comps.HelloWorld)
 
 export default defineComponent({
   name: 'ProjHelloWorld',
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import * as Vue from 'vue'
 import { preFetchLib, bindVueRuntime } from 'hel-micro'
 
-const VueMod: any = Vue
-console.log(VueMod)
-console.log(VueMod.Module)
-bindVueRuntime({ Vue: VueMod })
+bindVueRuntime({ Vue })
 
 // eslint-disable-next-line
 async function main() {
-  // await preFetchLib('hel-tpl-remote-vue3-comps-ts')
+  await preFetchLib('hel-tpl-remote-vue3-comps-ts')
 
-  await preFetchLib('hel-tpl-remote-vue3-comps-ts', {
-    custom: {
-      host: 'http://localhost:7001'
-    }
-  })
+  // 本地联调
+  // await preFetchLib('hel-tpl-remote-vue3-comps-ts', {
+  //   custom: {
+  //     host: 'http://localhost:7001'
+  //   }
+  // })
 
   await import('./loadApp')
 }
